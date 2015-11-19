8a544f8b0ae8edbbbe2d2ce113fc24e044d679cc
Ryan <ryan@burgettweb.net>
2015-11-19 08:32:33 -0500

    Added dynamic backgrounds

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,30 @@
 /*global $*/
 
 $(document).ready(function() {
+    'use strict';
 
-    console.log('Hi!');
+    var getBackground = function() {
+
+        var backgrounds = [
+            'bg1.jpg',
+            'bg2.jpg',
+            'bg3.jpg',
+            'bg4.jpg'
+        ];
+
+        var num = 1;
+
+        for(var i = 0; i < 500; i++) {
+            num = parseInt(Math.random() * backgrounds.length + 1);
+            if(num > 0 && num < backgrounds.length + 1) {
+                break;
+            }
+        }
+
+        return backgrounds[num - 1];
+
+    };
+
+    $('body').css('background-image', 'url("/images/backgrounds/' + getBackground() + '")');
 
 });
